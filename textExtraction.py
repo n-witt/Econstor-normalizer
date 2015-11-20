45d91dab5897d884599e8843524df270419f75bd
Nils Witt <n.witt@zbw.eu>
2015-11-20 11:11:19 +0100

    multithreading version is working
    Unfortunatly, it performs badly

--- textExtraction.py
+++ textExtraction.py
@@ -5,66 +5,76 @@ Created on 19.11.2015
 '''
 import Queue
 import threading
-import time
 import multiprocessing
+import os
+import pdf2text
+import logging
 
-exitFlag = 0
+logging.getLogger().setLevel(logging.INFO)
 numThreads = multiprocessing.cpu_count()
 threadList = []
 for i in range(numThreads):
     threadList.append("Thread-" + str(i))
-    
-nameList = ["One", "Two", "Three", "Four", "Five"]
+workingDir = "files"    
 queueLock = threading.Lock()
-workQueue = Queue.Queue(10)
 threads = []
 threadID = 1
 
 class myThread (threading.Thread):
-    def __init__(self, threadID, name, q):
+    def __init__(self, name, q, wd):
         threading.Thread.__init__(self)
-        self.threadID = threadID
         self.name = name
         self.q = q
+        self.wd = wd
 
     def run(self):
         print("Starting " + self.name)
-        process_data(self.name, self.q)
+        process_data(self.q, self.wd)
         print("Exiting " + self.name)
 
-def process_data(threadName, q):
-    while not exitFlag:
+def process_data(q, wd):
+        while not q.empty():
+            try:
+                queueLock.acquire()
+                filename = q.get()
+                queueLock.release()
+                plaintext = pdf2text.convert_pdf_to_txt(wd + os.sep + filename)
+                plainFilename = filename + ".txt" 
+                if not os.path.exists(wd + os.sep + plainFilename):
+                    fd = open(wd + os.sep + plainFilename, "w+")
+                    fd.write(plaintext)
+                    logging.info(plainFilename + " written")
+            except Exception as e:
+                logging.info("I/O error({0}): {1}".format(e.errno, e.strerror))
+                continue
+
+def fillQueue(path):
+    if os.path.exists(path):
+        docs = os.listdir(path)
+        q = Queue.Queue()
         queueLock.acquire()
-        if not workQueue.empty():
-            data = q.get()
-            queueLock.release()
-            print("%s processing %s" % (threadName, data))
-        else:
-            queueLock.release()
-        time.sleep(1)
+        for doc in docs:
+            q.put(doc)
+        queueLock.release()
+        return q
+    else:
+        raise Exception("path was not a valid path")
 
 
-# Create new threads
-for tName in threadList:
-    thread = myThread(threadID, tName, workQueue)
-    thread.start()
-    threads.append(thread)
-    threadID += 1
+if __name__ == "__main__":
+    
+    q = fillQueue("files")
+    # Create new threads
+    for tName in threadList:
+        thread = myThread(tName, q, workingDir)
+        thread.start()
+        threads.append(thread)
 
-# Fill the queue
-queueLock.acquire()
-for word in nameList:
-    workQueue.put(word)
-queueLock.release()
+    # Wait for queue to empty
+    #while not q.empty():
+    #    pass
 
-# Wait for queue to empty
-while not workQueue.empty():
-    pass
-
-# Notify threads it's time to exit
-exitFlag = 1
-
-# Wait for all threads to complete
-for t in threads:
-    t.join()
-print("Exiting Main Thread")
+    # Wait for all threads to complete
+    for t in threads:
+        t.join()
+    print("Exiting Main Thread")
